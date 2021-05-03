0c8ddcbb6239a90bd7b1e7d6c90af0b3fd377f2b
jonashj <jonasbhjulstad@gmail.com>
2021-05-03 22:53:47 +0200

    Working MCMC!

--- Custom_MCMC/Plot/ParamPlot.py
+++ Custom_MCMC/Plot/ParamPlot.py
@@ -4,14 +4,27 @@ import pandas as pd
 
 if __name__ == '__main__':
     print('test')
+    
     df_param = pd.read_csv("../Data/param_out.csv", names=['alpha', 'beta'], header=None)
     df_weights = pd.read_csv("../Data/weight_out.csv", names=['Weights'], header=None)
 
-    df_param = df_param.iloc[100:]
+    discard = False
+    if discard:
+        a = 1000
+        print("Discarding first %i".format(a))
+        df_param = df_param[a:]
     
-    df_weights = df_weights[df_weights['Weights'] < 10]
-    print(df_weights)
-    # df_weights['Weights'] = np.exp(df_weights['Weights'] - max(df_weights['Weights']))
-    plt.hist(df_param, label=df_param.columns, bins=100)
-    plt.legend()
+    R0 = 1.2
+    alpha = 1./9
+    beta = alpha*R0
+    print(df_weights.iloc[5:20])
+    fig, ax = plt.subplots(2)
+    ax[0].axvline(alpha)
+    ax[1].axvline(beta)
+    # df_weights['Weights'] = np. exp(df_weights
+    # ['Weights'] - max(df_weights['Weights']))
+    ax[0].hist(df_param['alpha'], label=df_param.columns[0], bins=30)
+    ax[1].hist(df_param['beta'], label=df_param.columns[1], bins=30)
+    ax[0].legend()
+    ax[1].legend()
     plt.show()
